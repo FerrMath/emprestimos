d0ad64fab327cad410961b3d4753a6c84030bc13
Matheus A <maf_dev@hotmail.com>
2024-04-11 22:59:20 -0300

    Add FAQ to emprestimos page and minor layout fix

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const menuBtn = document.querySelector('.menu_btn');
+const faqItens = document.querySelectorAll('.faq_item');
+
+// FAQ
+faqItens.forEach(item => {
+	item.addEventListener('click', ()=> {
+		item.classList.toggle('hidden')
+	})
+})
 
 // Dropdown menu
 menuBtn.addEventListener("click", ()=> {
